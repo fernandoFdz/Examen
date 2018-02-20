9643694b4209d98a2f5638e61977650d7162cdef
fernandoFdz <31115300+fernandoFdz@users.noreply.github.com>
2018-02-20 09:20:02 -0600

    examen
    examen corregido

---
+++ Examen/resource.h
@@ -0,0 +1,40 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Developer Studio generated include file.
+// Used by Tipico 1.rc
+//
+#define IDC_MYICON                      2
+#define IDC_BTN1                        3
+#define IDC_CREA                        3
+#define IDC_BTN2                        4
+#define IDC_OPRIMIR                     4
+#define IDC_BTN3                        5
+#define IDC_OCULTAR                     5
+#define IDC_BTN4                        6
+#define IDC_MOVER                       6
+#define IDC_BTN5                        7
+#define IDC_RESTAURAR                   7
+#define IDC_CREADO                      8
+#define IDD_TIPICO1_DIALOG              102
+#define IDD_ABOUTBOX                    103
+#define IDS_APP_TITLE                   103
+#define IDM_ABOUT                       104
+#define IDM_EXIT                        105
+#define IDS_HELLO                       106
+#define IDI_TIPICO1                     107
+#define IDI_SMALL                       108
+#define IDC_TIPICO1                     109
+#define IDR_MAINFRAME                   128
+#define IDD_ACCION                      131
+#define IDM_ACCION                      32771
+#define IDC_STATIC                      -1
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        132
+#define _APS_NEXT_COMMAND_VALUE         32772
+#define _APS_NEXT_CONTROL_VALUE         1000
+#define _APS_NEXT_SYMED_VALUE           110
+#endif
+#endif
